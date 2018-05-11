be4880d4bf7b49ab2af6180773cf9280c86f6ea1
Ben Parizek <ben@barrelstrengthdesign.com>
2018-05-11 13:24:30 -0400

    Refactors application folder structure

--- src/integrations/sproutreports/datasources/Categories.php
+++ src/integrations/sproutreports/datasources/Categories.php
@@ -2,8 +2,8 @@
 
 namespace barrelstrength\sproutreportscategories\integrations\sproutreports\datasources;
 
-use barrelstrength\sproutbase\sproutreports\contracts\BaseDataSource;
-use barrelstrength\sproutbase\sproutreports\elements\Report;
+use barrelstrength\sproutbase\app\reports\contracts\BaseDataSource;
+use barrelstrength\sproutbase\app\reports\elements\Report;
 use craft\records\Category as CategoryRecord;
 use craft\records\Entry as EntryRecord;
 use craft\db\Query;
